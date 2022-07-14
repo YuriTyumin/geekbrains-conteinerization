b5ee50e0966c59e69564c93c37da443ccf3eb8d4
YuriTyumin <43612457+YuriTyumin@users.noreply.github.com>
2022-07-14 03:07:06 +0300

    Update secret_create.sh

--- homework/4.resources-and-persistence/secret_create.sh
+++ homework/4.resources-and-persistence/secret_create.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-kubectl create ns pg
-kubectl --namespace=pg create secret generic pgpass --from-literal=key1=testpassword
+kubectl create secret generic pgpass --from-literal=key1=testpassword
